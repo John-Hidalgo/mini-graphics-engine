9f0b080f33424ba73142c0eeec76ba6e280a9bdc
John Hidalgo <2297119@csfoy.ca>
2025-11-03 20:38:25 -0500

    10.5: easy cam for deferred rendering scene

--- src/DeferredRenderer.cpp
+++ src/DeferredRenderer.cpp
@@ -1,17 +1,28 @@
 #include "DeferredRenderer.h"
 #include <glm/gtc/matrix_transform.hpp>
 
-DeferredRenderer::DeferredRenderer(float leftPanel,float RightPanel, float BottomPanel)
-	: leftPanelWidth(leftPanel), rightPanelWidth(RightPanel),bottomPanelHeight(BottomPanel){}
+DeferredRenderer::DeferredRenderer(float leftPanel, float RightPanel, float BottomPanel)
+	: leftPanelWidth(leftPanel), rightPanelWidth(RightPanel), bottomPanelHeight(BottomPanel) {}
 
 void DeferredRenderer::setup() {
 	ofDisableArbTex();
-	//ofEnableDepthTest();
 	deferred.setup();
 	cube = Mesh::Cube();
+	setupFixedCamera();
+	setupEasyCamera();
+}
 
-	proj = glm::perspective(glm::radians(45.0f), 1500.0f / 1000.0f, 0.1f, 100.0f);
-	view = glm::lookAt(glm::vec3(0, 10, 10), glm::vec3(0,0,0), glm::vec3(0,1,0));
+void DeferredRenderer::setupFixedCamera() {
+	fixedProj = glm::perspective(glm::radians(45.0f), 1500.0f / 1000.0f, 0.1f, 100.0f);
+	fixedView = glm::lookAt(glm::vec3(0, 10, 10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+}
+
+void DeferredRenderer::setupEasyCamera() {
+	easyCam.setDistance(15.0f);
+	easyCam.setNearClip(0.1f);
+	easyCam.setFarClip(100.0f);
+	easyCam.setPosition(0, 10, 10);
+	easyCam.lookAt(glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 }
 
 void DeferredRenderer::update() {
@@ -23,8 +34,8 @@ void DeferredRenderer::draw() {
 	int windowW = ofGetWidth();
 	int windowH = ofGetHeight();
 
-	float scaleW = 0.529f;//(1700 - (rightPanelWidth + leftPanelWidth))/1700;
-	float scaleH = 0.843f;//(1024 - bottomPanelWidth) 1024;
+	float scaleW = 0.56f;
+	float scaleH = 0.839f;
 
 	int renderW = int(windowW * scaleW);
 	int renderH = int(windowH * scaleH);
@@ -34,12 +45,11 @@ void DeferredRenderer::draw() {
 
 	std::vector<Mesh> cubes(9, cube);
 	std::vector<glm::mat4> models(9);
-	int floorSize = 5;
+	int floorSize = 20;
 	float cubeSize = 1.0f;
 	for (int x = -floorSize; x <= floorSize; ++x) {
 		for (int z = -floorSize; z <= floorSize; ++z) {
-			glm::mat4 model = glm::translate(glm::mat4(1.0f),
-											 glm::vec3(x * cubeSize, -cubeSize / 2.0f, z * cubeSize));
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),glm::vec3(x * cubeSize, -cubeSize / 2.0f, z * cubeSize));
 			models.push_back(model);
 			cubes.push_back(cube);
 		}
@@ -47,14 +57,44 @@ void DeferredRenderer::draw() {
 	float spacing = 3.0f;
 	for (int x = -1; x <= 2; ++x) {
 		for (int z = -1; z <= 1; ++z) {
-			glm::mat4 model = glm::translate(glm::mat4(1.0f),
-											 glm::vec3(x * spacing, 1.5f, z * spacing));
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),glm::vec3(x * spacing, 1.5f, z * spacing));
 			model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
 			models.push_back(model);
 			cubes.push_back(cube);
 		}
 	}
+	glm::mat4 currentView, currentProj;
+	if (useEasyCam) {
+		ofPushView();
+		ofViewport(viewportX, viewportY, renderW, renderH);
+		easyCam.begin();
+		currentView = easyCam.getModelViewMatrix();
+		currentProj = easyCam.getProjectionMatrix();
+		easyCam.end();
+		ofPopView();
+	}
+	else {
+		currentView = fixedView;
+		currentProj = fixedProj;
+	}
+	
 	deferred.shadowPass(cubes, models);
-	deferred.geometryPass(cubes, models, view, proj);
-	deferred.lightingPass(view, proj,viewportX, viewportY, renderW, renderH);
+	deferred.geometryPass(cubes, models, currentView, currentProj);
+	deferred.lightingPass(currentView, currentProj, viewportX, viewportY, renderW, renderH);
+}
+
+void DeferredRenderer::setFixedCameraPosition(const glm::vec3& position, const glm::vec3& target) {
+	fixedView = glm::lookAt(position, target, glm::vec3(0, 1, 0));
+}
+
+void DeferredRenderer::resetFixedCamera() {
+	setupFixedCamera();
+}
+
+glm::mat4 DeferredRenderer::getCurrentViewMatrix() const {
+	return useEasyCam ? easyCam.getModelViewMatrix() : fixedView;
+}
+
+glm::mat4 DeferredRenderer::getCurrentProjectionMatrix() const {
+	return useEasyCam ? easyCam.getProjectionMatrix() : fixedProj;
 }
--- src/DeferredRenderer.h
+++ src/DeferredRenderer.h
@@ -7,20 +7,32 @@
 
 class DeferredRenderer {
 public:
-	DeferredRenderer(float leftPanel,float RightPanel, float BottomPanel);
+	DeferredRenderer(float leftPanel, float RightPanel, float BottomPanel);
 	void setup();
 	void update();
 	void draw();
+	void enableEasyCam(bool enable) { useEasyCam = enable; }
+	void toggleCamera() { useEasyCam = !useEasyCam; }
+	ofEasyCam& getCamera() { return easyCam; }
+	bool isUsingEasyCam() const { return useEasyCam; }
+	void setFixedCameraPosition(const glm::vec3& position, const glm::vec3& target = glm::vec3(0, 0, 0));
+	void resetFixedCamera();
 
 private:
 	Mesh cube;
 	Deferred deferred = Deferred(1000, 700);
+	ofEasyCam easyCam;
 	
 	float angle = 0.0f;
-	glm::mat4 view;
-	glm::mat4 proj;
+	glm::mat4 fixedView;
+	glm::mat4 fixedProj;
 	float leftPanelWidth = 220.0f;
 	float rightPanelWidth = 540.0f;
 	float bottomPanelHeight = 160.0f;
+	bool useEasyCam = true;
 
+	void setupFixedCamera();
+	void setupEasyCamera();
+	glm::mat4 getCurrentViewMatrix() const;
+	glm::mat4 getCurrentProjectionMatrix() const;
 };
